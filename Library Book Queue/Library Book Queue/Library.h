63073a140bde7b82c17b21ce882969fe3dca6db5
Bluemints <warwick812@gmail.com>
2018-03-14 16:13:35 -0500

    added Date class

--- Library Book Queue/Library Book Queue/Library.h
+++ Library Book Queue/Library Book Queue/Library.h
@@ -1,9 +1,15 @@
 #pragma once
+#include"Date.h"
 #include <vector>
+
+using namespace std;
+
 class Library
 {
 private:
-	
+	void pass_on(string title);
 public:
-
+	void addBook(string bookName);
+	void addEmployee(string Name);
+	void circulateBook(string title, Date startDate);
 };
---
+++ Library Book Queue/Library Book Queue/StringTokenizer.cpp
@@ -0,0 +1,2 @@
+#include"StringTokenizer.h"
+
