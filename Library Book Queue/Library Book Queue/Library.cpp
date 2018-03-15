169e20f7fe393695af552bef9aaa1958dfae57a5
mikelhomeless <mthomer1027@gmail.com>
2018-03-15 11:06:49 -0500

    Updated Functions
    Updated statements that called get_name() and set_name to get_title() and set_title()

--- Library Book Queue/Library Book Queue/Library.cpp
+++ Library Book Queue/Library Book Queue/Library.cpp
@@ -10,7 +10,7 @@ void Library::addBook(string bookName) {
 	if (bookName == "")
 		throw invalid_argument("Error: empty string");
 	Book newBook;
-	newBook.set_name(bookName);
+	newBook.set_title(bookName);
 	archBooks.push_back(newBook);
 }
 
@@ -19,15 +19,15 @@ void Library::addEmployee(string Name) {
 		throw invalid_argument("Error: empty string");
 	Employee newEmp;
 	newEmp.name = Name;
-	// If there are currently books in the circulating books vector, place a pointer to the employee into the book’s queue by calling the book class’s push function
+	// If there are currently books in the circulating books vector, place a pointer to the employee into the bookâ€™s queue by calling the book classâ€™s push function
 }
 
 void Library::circulateBook(string title, Date startDate) {
 	Book newBook;
 	bool found = false;
 	for (unsigned int i = 0; i < archBooks.size(); i++) {
-		if (archBooks[i].get_name() == title) {
-			newBook.set_name(archBooks[i].get_name());
+		if (archBooks[i].get_title() == title) {
+			newBook.set_title(archBooks[i].get_title());
 			newBook.set_startDate(startDate);
 			circBooks.push_back(newBook);
 			archBooks.erase(archBooks.begin() + (i - 1));
