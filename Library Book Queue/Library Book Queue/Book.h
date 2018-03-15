657563991cc46d7142b9bb5c774b0f9aa00bf915
mikelhomeless <mthomer1027@gmail.com>
2018-03-15 10:38:50 -0500

    Added Geters and Setters and more
    added last_pass date, moved pass_on function from protected to public,
    	added Object constructors, changed set_name to set_title, changed get_name to get_title,
    	added getters and setters for end_date and last_pass

--- Library Book Queue/Library Book Queue/Book.h
+++ Library Book Queue/Library Book Queue/Book.h
@@ -1,22 +1,42 @@
 #pragma once
 #include"Date.h"
+#include "priority_queue"
 #include"Employee.h"
 #include<vector>
-using namespace std;
+/*What has changed
+	added last_pass date, moved pass_on function from protected to public
+	added Object constructors, changed set_name to set_title, changed get_name to get_title
+	added getters and setters for end_date and last_pass*/
+
+/* TODO
+	Update getter and setter functions in CPP file
+	Merge with updated version of priority_queue
+	define pass_on
+	add push Function
+	add Exceptions
+	*/
 
 class Book
 {
 private:
-	Date start_date;
-	Date end_date;
-	string name;
-	Employee* priority_queue;
+	Date start_date, last_pass, end_date;
+	std::string title;
+	//priority_queue<Employee*> queue;
 	Employee* emp_with_book;
-protected:
-	void pass_on(Date curdate);
 public:
-	void set_name(string n) {name = n;}
-	string get_name() {return name;}
-	void set_startDate(Date s) {start_date = s;}
-	Date get_startDate() {return start_date;}
+	Book();
+	Book(std::string);
+
+	void pass_on(Date);
+
+	// Getters and Setters
+	void set_title(std::string);
+	std::string get_title();
+	void set_startDate(Date);
+	Date get_startDate();
+	void set_lastPass(Date);
+	Date get_lastPass();
+	void set_endDate(Date);
+	Date get_endDate();
+
 };
