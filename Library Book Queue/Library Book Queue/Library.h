f9838e402c4ea1ea5f5b2016e1dc5a101ab8aaf9
mikelhomeless <mthomer1027@gmail.com>
2018-03-15 11:51:38 -0500

    Changes to pass_on function
    	moved pass_on from private to public
    	changed pass on from void to bool
    	updated pass_on to accept two parameters; title and current date
    	added declaration for update queues function

--- Library Book Queue/Library Book Queue/Library.h
+++ Library Book Queue/Library Book Queue/Library.h
@@ -4,6 +4,15 @@
 #include"Book.h"
 #include <vector>
 
+/*What has changed
+	moved pass_on from private to public
+	changed pass on from void to bool
+	updated pass_on to accept two parameters; title and current date
+	added declaration for update queues function*/
+
+/*TODO
+	add definition for update_queues function*/
+
 using namespace std;
 
 class Library
@@ -12,9 +21,10 @@ private:
 	vector<Employee> Employees;
 	vector<Book> circBooks;
 	vector<Book> archBooks;
-	void pass_on(string title);
 public:
+	bool pass_on(string title, Date curdate);
 	void addBook(string bookName);
 	void addEmployee(string Name);
 	void circulateBook(string title, Date startDate);
+	void update_queues(Employee*);
 };
