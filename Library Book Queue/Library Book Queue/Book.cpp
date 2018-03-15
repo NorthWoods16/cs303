19589410a9051a4c3b2114b46c3fdda1f879fa24
mikelhomeless <mthomer1027@gmail.com>
2018-03-15 10:40:19 -0500

    Added implementation of Getters and setters

--- Library Book Queue/Library Book Queue/Book.cpp
+++ Library Book Queue/Library Book Queue/Book.cpp
@@ -1,1 +1,21 @@
 #include "Book.h"
+
+Book::Book() : title("") {}
+
+Book::Book(std::string s) : title(s) {}
+
+void Book::set_title(std::string t) { title = t; }
+
+std::string Book::get_title() { return title; }
+
+void Book::set_startDate(Date s) { start_date = s; }
+
+Date Book::get_startDate() { return start_date; }
+
+void Book::set_lastPass(Date l) { last_pass = l; }
+
+Date Book::get_lastPass() { return last_pass; }
+
+void Book::set_endDate(Date e) { end_date = e; }
+
+Date Book::get_endDate() { return end_date; }
