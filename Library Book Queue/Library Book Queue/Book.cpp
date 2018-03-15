fd16b16507a34c875bb9fed071e55b32f7f40188
mikelhomeless <mthomer1027@gmail.com>
2018-03-15 11:01:18 -0500

    Defined pass_on function

--- Library Book Queue/Library Book Queue/Book.cpp
+++ Library Book Queue/Library Book Queue/Book.cpp
@@ -4,6 +4,34 @@ Book::Book() : title("") {}
 
 Book::Book(std::string s) : title(s) {}
 
+bool Book::pass_on(Date curdate) {
+	if (emp_with_book != NULL) {
+		emp_with_book->retaintingTime = last_pass - curdate;
+		last_pass = curdate;
+
+		if (!queue.empty()) {
+			emp_with_book = queue.front();
+			queue.pop();
+			emp_with_book->waitingTime = curdate - start_date;
+			return true;
+		}
+		else {
+			emp_with_book = NULL;
+			return false;
+		}
+	}
+	else {
+		if (!queue.empty()) {
+			emp_with_book = queue.front();
+			queue.pop();
+			return true;
+		}
+		else {
+			//Throw error
+		}
+	}
+}
+
 void Book::set_title(std::string t) { title = t; }
 
 std::string Book::get_title() { return title; }
@@ -19,3 +47,4 @@ Date Book::get_lastPass() { return last_pass; }
 void Book::set_endDate(Date e) { end_date = e; }
 
 Date Book::get_endDate() { return end_date; }
+
