fbe15f81ad58d4d80b038295f60ad1d5f62a9e2c
NorthWoods16 <andrewdale166@gmail.com>
2018-03-15 20:35:19 -0500

    final commit

--- Library Book Queue/Library Book Queue/Book.cpp
+++ Library Book Queue/Library Book Queue/Book.cpp
@@ -1,55 +1,55 @@
-#include "Book.h"
-
-Book::Book() : title("") {}
-
-Book::Book(std::string s) : title(s) {}
-
-bool Book::pass_on(Date curdate) {
-	if (emp_with_book != NULL) {
-		emp_with_book->retaintingTime = last_pass - curdate;
-		last_pass = curdate;
-
-		if (!queue.isempty()) {
-			emp_with_book = queue.front();
-			queue.pop();
-			emp_with_book->waitingTime = curdate - start_date;
-			return true;
-		}
-		else {
-			emp_with_book = NULL;
-			return false;
-		}
-	}
-	else {
-		if (!queue.isempty()) {
-			emp_with_book = queue.front();
-			queue.pop();
-			return true;
-		}
-		else {
-			//Throw error
-		}
-	}
-}
-
-void Book::push(Employee* e, int p){
-	queue.push(e, p);
-}
-
-void Book::set_title(std::string t) { title = t; }
-
-std::string Book::get_title() { return title; }
-
-void Book::set_startDate(Date s) { start_date = s; }
-
-Date Book::get_startDate() { return start_date; }
-
-void Book::set_lastPass(Date l) { last_pass = l; }
-
-Date Book::get_lastPass() { return last_pass; }
-
-void Book::set_endDate(Date e) { end_date = e; }
-
-Date Book::get_endDate() { return end_date; }
-
-Employee* Book::getHolder() { return emp_with_book; }
+//#include "Book.h"
+//
+//Book::Book() : title("") {}
+//
+//Book::Book(std::string s) : title(s) {}
+//
+//bool Book::pass_on(Date curdate) {
+//	if (emp_with_book != NULL) {
+//		emp_with_book->retaintingTime = last_pass - curdate;
+//		last_pass = curdate;
+//
+//		if (!queue.isempty()) {
+//			emp_with_book = queue.front();
+//			queue.pop();
+//			emp_with_book->waitingTime = curdate - start_date;
+//			return true;
+//		}
+//		else {
+//			emp_with_book = NULL;
+//			return false;
+//		}
+//	}
+//	else {
+//		if (!queue.isempty()) {
+//			emp_with_book = queue.front();
+//			queue.pop();
+//			return true;
+//		}
+//		else {
+//			//Throw error
+//		}
+//	}
+//}
+//
+//void Book::push(Employee* e, int p){
+//	queue.push(e, p);
+//}
+//
+//void Book::set_title(std::string t) { title = t; }
+//
+//std::string Book::get_title() { return title; }
+//
+//void Book::set_startDate(Date s) { start_date = s; }
+//
+//Date Book::get_startDate() { return start_date; }
+//
+//void Book::set_lastPass(Date l) { last_pass = l; }
+//
+//Date Book::get_lastPass() { return last_pass; }
+//
+//void Book::set_endDate(Date e) { end_date = e; }
+//
+//Date Book::get_endDate() { return end_date; }
+//
+//Employee* Book::getHolder() { return emp_with_book; }
