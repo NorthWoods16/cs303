edcdcae98e98adf35d42ed16aead13309d1d4f38
John Winne <warwick812@gmail.com>
2018-03-19 18:22:55 -0500

    Update Book.h

--- Library Book Queue/Library Book Queue/Book.h
+++ Library Book Queue/Library Book Queue/Book.h
@@ -21,12 +21,16 @@ private:
 	Priority_Queue queue;
 	Employee* emp_with_book;
 public:
+	//default book constructor
 	Book();
+	//book contstructor that sets the title of the book
 	Book(string name);
 	Book(Book&);
-
+	//sets book to next person in queue
 	bool pass_on(Date);
+	//calls queues push function
 	void push(Employee* e, int priority);
+	//
 	bool contains(Employee* emp);
 	void update(Employee* e, int p);
 	Book& operator=(Book& rhs);
