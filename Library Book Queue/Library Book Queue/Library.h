7fc548c571bda9221fe6a32f45f55894e1daaa06
mikelhomeless <mthomer1027@gmail.com>
2018-03-20 13:54:35 -0500

    Changing employee vector to contain pointers

--- Library Book Queue/Library Book Queue/Library.h
+++ Library Book Queue/Library Book Queue/Library.h
@@ -14,21 +14,14 @@ using namespace std;
 class Library
 {
 private:
-	//vector holds employees that will check out books
-	vector<Employee> Employees;
-	//vector holds books that are circulation
+	vector<Employee*> Employees;
 	vector<Book> circBooks;
-	//vector holds archived books
 	vector<Book> archBooks;
+	int getPriority(Employee&);
 public:
-	//locates a book in circulating book vector and calls the book method "passon"
 	bool pass_on(string title, Date curdate);
-	//adds a book object to the archived books vector
 	void addBook(string bookName);
-	//adds an employee to the employee vector 
 	void addEmployee(string Name);
-	//book is moved from archived book vector into circulating book vector
 	void circulateBook(string title, Date startDate);
-	//updates book queues
 	void update_queues(Employee* e);
 };
