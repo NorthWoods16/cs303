07e40e83aa7993e715a336b57a24e1cdbcee4ea7
mikelhomeless <mthomer1027@gmail.com>
2018-03-20 14:04:55 -0500

    Major Bug Fixes/ Cleaning
    Fixed bug that didn't set variable for employee who has the book
    Cleaned up functions to be more readable
    Comments Added

--- Library Book Queue/Library Book Queue/Book.cpp
+++ Library Book Queue/Library Book Queue/Book.cpp
@@ -1,95 +1,73 @@
 #include "Book.h"
 #include "Priority_Queue.h"
 
+/*TODO
+Clean all functions
+Test all functions*/
 Book::Book() : title("") {}
 
-Book::Book(std::string s) : title(s) {}
+Book::Book(std::string s) : title(s), emp_with_book(nullptr) {}
 
-Book::Book(Book& other) {
+Book::Book(const Book& other) {
 	*this = other;
 }
 
+// Passes the book on to the next person in the queue
 bool Book::pass_on(Date curdate) {
-	//if an employee does have the book,employees rtime is updated
-	if (emp_with_book != nullptr) {
-		emp_with_book->retaintingTime = last_pass - curdate;
-		last_pass = curdate;
-		//if queue is not empty, book wielder is set to person at the front of the queue
-		if (!queue.isempty()) {
-			emp_with_book = queue.get_front();
-			queue.pop();
-			//book wielder wait time is set by subtracting current date from the start date
-			emp_with_book->waitingTime = curdate - start_date;
-			//return true after successful pass on
-			return true;
-		}
-		else {
-			//if queue is empty, book wielder is null
-			emp_with_book = nullptr;
-			return false;
-		}
+
+	// Raise error if no one to pass on to
+	if (emp_with_book == nullptr)
+		throw invalid_argument("Error: No current holder exists");
+
+	// If none of the above conditions are met, pass the book on normally
+	emp_with_book->retaintingTime = curdate - last_pass;
+	last_pass = curdate;
+
+	if (!queue.isempty()) {
+		emp_with_book = queue.get_front();
+		queue.pop();
+		emp_with_book->waitingTime = curdate - start_date;
+		return true;
 	}
 	else {
-		//if book wielder is null and the queuue is not empty, book wielder is set to the person in the fron to the queue
-		if (!queue.isempty()) {
-			emp_with_book = queue.get_front();
-			queue.pop();
-			return true;
-		}
-		else {
-			//if book wielder is null but queue is not empty, there has been an error
-			throw invalid_argument("Error: Employee could not be found");
-		}
+		emp_with_book = nullptr;
+		return false;
 	}
 }
-//pushes employee and their priority into queue
-void Book::push(Employee* e, int p){
-	queue.push(e, p);
+
+// Pushes employee into the queue
+void Book::push(Employee* e, int p) {
+	if (emp_with_book == nullptr)
+		emp_with_book = e;
+	else
+		queue.push(e, p);
 }
-//checks to see if employee holds book
+
+// Returns bool for if employee is in the queue
 bool Book::contains(Employee* emp) {
+
 	return queue.search(emp);
+
 }
 
-void Book::set_title(std::string t) { title = t; }
-
-std::string Book::get_title() { return title; }
-//initalies start date and last pass for book
-void Book::set_startDate(Date s) { 
-	start_date = s; 
+// Sets the start date
+void Book::set_startDate(Date s) {
+	start_date = s;
 	last_pass = s;
 }
 
-Date Book::get_startDate() { return start_date; }
-
-
-void Book::set_lastPass(Date l) { last_pass = l; }
-
-
-Date Book::get_lastPass() { return last_pass; }
-
-
-void Book::set_endDate(Date e) { end_date = e; }
-
-
-Date Book::get_endDate() { return end_date; }
-
-
-Employee* Book::getHolder() { return emp_with_book; }
-
-//calls queues update method, updates employees priority in queue
+// Updates the priority of an item in the book's queue
 void Book::update(Employee* e, int p) {
 	queue.update(e, p);
 }
 
-//copy constructor override
-Book& Book::operator=(Book& RHS) {
+// Assignment operator
+Book& Book::operator=(const Book& RHS) {
 	title = RHS.title;
 	start_date = RHS.start_date;
 	last_pass = RHS.last_pass;
 	end_date = RHS.end_date;
 	emp_with_book = RHS.emp_with_book;
-	while (!queue.isempty())
-		queue.pop();
+	queue = RHS.queue;
 	return *this;
 }
