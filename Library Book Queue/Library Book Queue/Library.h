b632645334105bb2fae13ec8fa1656e83b99f16e
John Winne <warwick812@gmail.com>
2018-03-20 19:14:19 -0500

    commenting

--- Library Book Queue/Library Book Queue/Library.h
+++ Library Book Queue/Library Book Queue/Library.h
@@ -4,24 +4,29 @@
 #include"Book.h"
 #include <vector>
 
-/*What has changed
-	added definition for update_queues*/
-
-/*TODO*/
 
 using namespace std;
 
 class Library
 {
 private:
+	//vector holds employee pointers
 	vector<Employee*> Employees;
+	//vector for books in circulation
 	vector<Book> circBooks;
+	//vector for arhcived books
 	vector<Book> archBooks;
+	//employee priority
 	int getPriority(Employee*);
 public:
+	//passes book on to next employee of highest priority
 	bool pass_on(string title, Date curdate);
+	//adds a book
 	void addBook(string bookName);
+	//adds employee
 	void addEmployee(string Name);
+	//puts book into circulation
 	void circulateBook(string title, Date startDate);
+	//updates book queues
 	void update_queues(Employee* e);
 };
